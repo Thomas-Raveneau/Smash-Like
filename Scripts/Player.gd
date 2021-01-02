c16740b543bd55f2c98071dd4efc17a133f3f8fa
Maxime Drelon <maxime.drelon@epitech.eu>
2021-01-02 23:17:12 +0100

    [ADD] - working movement without gravity

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,27 +1,29 @@
 extends KinematicBody2D
 
-export (int) var speed = 400
-export (int) var jump_speed = -1800
-export (int) var gravity = 4000
+export (int) var SPEED = 400
+export (int) var JUMP_FORCE = -1800
+export (int) var GRAVITY = 4000
 
 var velocity = Vector2.ZERO
 
 remote func _set_position(pos):
 	global_position = pos
 
-func get_input():
-	velocity.x = 0
-	if Input.is_action_pressed("walk_right"):
-		velocity.x += speed
-	if Input.is_action_pressed("walk_left"):
-		velocity.x -= speed
-
 func _physics_process(delta):
+	velocity = Vector2()
+
+	if Input.is_action_pressed("move_right"):
+		velocity.x += SPEED
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= SPEED
+	
+#	velocity.y += GRAVITY * delta
+	
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = JUMP_FORCE
+	
+	if velocity != Vector2():
 		if is_network_master():
-			get_input()
-			velocity.y += gravity * delta
 			velocity = move_and_slide(velocity, Vector2.UP)
-			if Input.is_action_just_pressed("jump"):
-				if is_on_floor():
-					velocity.y = jump_speed
 		rpc_unreliable("_set_position", global_position)
