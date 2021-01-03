fd2f762c1155858373c4540d8a642baedc1a4937
Maxime Drelon <maxime.drelon@epitech.eu>
2021-01-03 22:45:20 +0100

    [MERGE] - merge player

--- Scenes/Stages/Stage1/Stage.gd
+++ Scenes/Stages/Stage1/Stage.gd
@@ -5,7 +5,7 @@ var camera
 func _ready():
 	if (get_tree().is_network_server()):
 		get_node("Player2").set_network_master(get_tree().get_network_connected_peers()[0])
-		camera = get_node("Player2/Camera2D")
+		camera = get_node("Player/Camera2D")
 		camera._set_current(true)
 	else:
 		get_node("Player2").set_network_master(get_tree().get_network_unique_id())
